a78caccd6c65553f87e54d0efbbcb794d4f148ac
alluriprasad <alluriprasad@HDDLBGKIOSK13.Virtusa.com>
2019-06-14 11:29:57 +0530

    nabsdjghjbdfnbv

---
+++ src/main/java/test/bell/prasad.java
@@ -0,0 +1,18 @@
+package test.bell;
+
+public class prasad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+int a,b,c;
+a=10;
+b=12;
+c=a+b;
+System.out.println(c);
+System.out.println("result");
+
+
+	}
+
+}
